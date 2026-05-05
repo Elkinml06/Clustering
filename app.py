a5ed21a3f64770cbc7df24ee1cc6e47be4519022
Elkin <elkin1506ml@gmail.com>
2026-05-05 08:00:48 -0500

    Integrate PCA and scaling for clustering visualization
    Added PCA and StandardScaler for dimensionality reduction and scaling. Updated scatter plot visualization to use PCA components.

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.metrics import silhouette_score, davies_bouldin_score
 from model import preprocessing, clustering
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
 
 st.title("Clustering App")
 
@@ -32,20 +34,37 @@ model, labels = clustering(X,n_clusters,linkage)
 
 df["Cluster"] = labels
 
-if (len(features) == 2) and (df[features[0]].dtype in ['int64', 'float64']) and (df[features[1]].dtype in ['int64', 'float64']):
+fig, ax = plt.subplots()
+pca = PCA(n_components=2)
+numerical = df.select_dtypes(include=['int64', 'float64']).columns
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(df[numerical])
+X_pca = pca.fit_transform(X_scaled)
+ax.scatter(
+    X_pca[:,0],
+    X_pca[:,1],
+    c=df["Cluster"]
+)
+
+ax.set_xlabel('Component 1')
+ax.set_ylabel('Component 2')
+st.pyplot(fig)
+st.subheader("Clustered Data")
+st.dataframe(df)
+st.pyplot(fig)
+
+if (len(features) == 2) & (df[features[0]].dtype == 'int64') & (df[features[1]].dtype == 'int64') | (df[features[0]].dtype == 'float64') & (df[features[1]].dtype == 'float64'):
     fig, ax = plt.subplots()
     ax.scatter(
         df[features[0]],
         df[features[1]],
-        c=df["Cluster"],
-        cmap='viridis'
+        c=df["Cluster"]
     )
     ax.set_xlabel(features[0])
     ax.set_ylabel(features[1])
+
     st.pyplot(fig)
 else:
     st.info("Select exactly 2 numerical features for visualization")
-
 st.subheader("Clustered Data")
 st.dataframe(df)
-
